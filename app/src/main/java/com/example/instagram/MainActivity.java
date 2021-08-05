1b4aadaca33f41c5f261792a06f5ea378fb93c1d
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-08-05 19:14:44 -0300

    Adding an itent to change activity to the Grid Profile

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnProfile = findViewById(R.id.btn_person);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        btnProfile.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, GridProfile.class));
+        });
     }
 }
