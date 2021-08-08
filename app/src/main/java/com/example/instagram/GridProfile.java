39fe4787024ee2ab579dfc3e2cf032bec421713c
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-08-07 21:35:56 -0300

    Adding a new funcition to the intente nav bottom on grid activity main

--- app/src/main/java/com/example/instagram/GridProfile.java
+++ app/src/main/java/com/example/instagram/GridProfile.java
@@ -2,13 +2,27 @@ package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class GridProfile extends AppCompatActivity {
+
+    private ImageView btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_profile);
+        btnHome = findViewById(R.id.btn_home);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnHome.setOnClickListener(view -> {
+            startActivity(new Intent(GridProfile.this, MainActivity.class));
+        });
     }
 }
